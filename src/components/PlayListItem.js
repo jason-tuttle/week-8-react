a4078a9fcb792d91f026a7bb94e4511392d8985a
Jason Tuttle <jason@hearspotbark.com>
2017-07-21 14:37:07 -0400

    clean up and styling

--- src/components/PlayListItem.js
+++ src/components/PlayListItem.js
@@ -8,11 +8,7 @@ import React from 'react';
 //   }
 
 export default class PlayListItem extends React.Component {
-  constructor(props) {
-    super(props)
-
-  }
-
+  
   render() {
     const {song} = this.props;
     return (
